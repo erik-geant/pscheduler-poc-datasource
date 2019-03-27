d32cda09b168aafeec33f74067d62f3e97bad424
Erik Reid <erik.reid@geant.org>
2019-03-27 04:02:24 +0100

    added latency time series

--- src/datasource.js
+++ src/datasource.js
@@ -131,18 +131,44 @@ export class GenericDatasource {
     );
   }
 
+  _owpingts2epoch(owpts) {
+    // cf. owamp/arithm64.c, OWPNum64ToTimespec
+    var OWPJAN_1970 = 0x83aa7e80;
+//    var epoch_seconds = (owpts >> 32) - OWPJAN_1970;
+    var upper_32 = owpts / Math.pow(2,32);
+    var epoch_seconds = (upper_32) - OWPJAN_1970;
+    epoch_seconds -= OWPJAN_1970;
+//    var nsec = ((owpts & 0xffffffff) * 1000000000) >> 32;
+    var lower_32 = owpts - upper_32;
+    var nsec = ((owpts - lower_32) * 1000000000) / Math.pow(2,32);
+    var msec = Math.floor(nsec/1000000);
+    var epoch_ts_with_ms = epoch_seconds + msec/1000.0;
+    return epoch_ts_with_ms;
+  }
+
+  owpingts2epoch(owpts) {
+    var OWPJAN_1970 = 0x83aa7e80;
+    var upper_32 = owpts / Math.pow(2,32);
+    var epoch_seconds = (upper_32) - OWPJAN_1970;
+    return Math.floor(epoch_seconds * 1000)/1000;
+  }
+
+
   make_latency_table(response) {
     var columns = [
-        {text: 'src-ts', type: 'integer'},
-        {text: 'dst-ts', type: 'integer'},
-        {text: 'delta', type: 'integer'}
+        {text: 'src-ts', type: 'time'},
+        {text: 'dst-ts', type: 'time'},
+        {text: 'delta', type: 'number'}
     ];
 
     var rows = _.map(response['raw-packets'], p => {
+        var src_ts = this.owpingts2epoch(p['src-ts']);
+        var dst_ts = this.owpingts2epoch(p['dst-ts']);
+        var delta = (p['dst-ts'] - p['src-ts'])/Math.pow(2,32);
         return [
-            p['src-ts'],
-            p['dst-ts'],
-            p['dst-ts']-p['src-ts']
+            new Date(src_ts), 
+            new Date(dst_ts),
+            Math.floor(delta * 1000000)/1000
         ];
     });
  
@@ -151,6 +177,15 @@ export class GenericDatasource {
       rows: rows,
       type: 'table'
     }
+  }
+
+  make_latency_timeseries(response) {
+    return _.map(response['raw-packets'], p => {
+        var src_ts = this.owpingts2epoch(p['src-ts']);
+        var dst_ts = this.owpingts2epoch(p['dst-ts']);
+        var delta = (p['dst-ts'] - p['src-ts'])/Math.pow(2,32);
+        return [Math.floor(delta * 1000000)/1000, src_ts * 1000];
+    });
   }
 
   make_throughput_table(response) {
@@ -208,16 +243,26 @@ console.log('query(options): ' + JSON.stringify(options));
 
 console.log('target: ' + JSON.stringify(target));
 console.log('ds: ' + JSON.stringify(ds));
+console.log('result: ' + JSON.stringify(r));
          
          if (target.test_type == 'latency') {
-            data = ds.make_latency_table(r);
+            if (target.type == 'table') {
+              data = ds.make_latency_table(r);
+            } else {
+              data = {
+                target: target.refId,
+                datapoints: ds.make_latency_timeseries(r)
+              }
+            }
          }
 
          if (target.test_type == 'throughput') {
-            data = ds.make_throughput_table(r);
+            if (target.type == 'table') {
+              data = ds.make_throughput_table(r);
+            }
          }
             
-         console.log('data: ' + JSON.stringify(data));
+console.log('data: ' + JSON.stringify(data));
          
          return {
              _request: test_parameters,
