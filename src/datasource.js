ca835e8760cad8d606480da992f660f7292aba00
Erik Reid <erik.reid@geant.org>
2019-05-14 12:40:11 +0200

    support multiple targets

--- src/datasource.js
+++ src/datasource.js
@@ -38,10 +38,40 @@ export class GenericDatasource {
     );
   }
 
-   
+  get_timeseries(target) {
+    var backend_request = {
+        withCredentials: this.withCredentials,
+        headers: this.headers,
+        url: this.url + '/measurements/timeseries',
+        method: 'POST',
+        data: {
+          mp: target.mp,
+          task: target.task
+        }
+    };
+
+    console.log('target: ' + JSON.stringify(target));
+    console.log('query request: ' + JSON.stringify(backend_request));
+
+    return this.backendSrv.datasourceRequest(backend_request).then(
+        r => {
+            if (r.status != 200) {
+                console.log('error, got status: ' + r.status);
+                return 'bad request';
+            }
+            return {
+              target: target.refId,
+              datapoints: _.map(r.data, p => {
+                return [p[0], 1000*p[1]];
+              })
+            };
+       }
+    );
+  }
+
   query(options) {
 
-//console.log('query(options): ' + JSON.stringify(options));
+    console.log('query(options): ' + JSON.stringify(options));
 
     var targets = options.targets.filter(t => !t.hide);
 
@@ -54,42 +84,20 @@ export class GenericDatasource {
         });
     }
 
-    // just use the first target, as an experiement (for now only)
+    var target_promises = _.map(targets, t => {
+      return this.get_timeseries(t);
+    });
 
-    var target = targets[0];
-
-    var backend_request = {
-        withCredentials: this.withCredentials,
-        headers: this.headers,
-        url: this.url + '/measurements/timeseries',
-        method: 'POST',
-        data: {
-          mp: target.mp,
-          task: target.task
+    return Promise.all(target_promises).then(r => {
+        return {
+            _request: {
+                range: options.range,
+                interval: options.interval,
+                maxDataPoints: options.maxDataPoints
+            },
+            data: r
         }
-    };
-    console.log('target: ' + JSON.stringify(target));
-    console.log('query request: ' + JSON.stringify(backend_request));
-
-
-    return this.backendSrv.datasourceRequest(backend_request).then(
-        r => {
-            if (r.status != 200) {
-                console.log('error, got status: ' + r.status);
-                return 'bad request';
-            }
-            var ts_data = {
-              target: target.refId,
-              datapoints: _.map(r.data, p => {
-                return [p[0], 1000*p[1]];
-              })
-            };
-            return {
-                _request: backend_request,
-                data: [ts_data]
-            };
-        }
-    );
+    });
   }
 
   annotationQuery(options) {
@@ -98,14 +106,6 @@ export class GenericDatasource {
 
   metricFindQuery(query) {
     console.log('metricFindQuery: IS THIS METHOD EVER CALLED?');
-/*
-    return Promise.resolve(
-        [
-            {text: 'm1', value: 'v1'},
-            {text: 'm2', value: 'v2'}
-        ]);
-*/
-
     return [
         {text: 'm1', value: 'v1'},
         {text: 'm2', value: 'v2'}
